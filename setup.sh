831eb486d33f64e68c43e581f40b5ec5262bc2ba
Byt3Danc3 <168964544+Byt3Danc3@users.noreply.github.com>
2024-05-05 20:49:16 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,14 @@ function line_break () {
     printf "\n"
 }
 
+line_break "update to ensure we can install"
+sudo apt update
+
+line_break "install auto-apt-proxy for those with apt-cacher-ng to drastically speed up rebuilds"
+sudo apt install auto-apt-proxy 
+
+line_break "run auto-apt-proxy, if cannot detect apt-cacher-ng then exit. Disable if not available"
+auto-apt-proxy  || exit
 
 line_break "Installing ansible via pip"
 sudo python3 -m pip install ansible
