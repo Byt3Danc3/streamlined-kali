742d1c5a63687335d3bcfdfaa10f7e5e97f61e8b
Byt3Danc3 <168964544+Byt3Danc3@users.noreply.github.com>
2024-06-09 16:14:15 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,8 +37,11 @@ sudo systemctl enable --now ssh.service
 sudo whoami > /dev/null
 
 line_break "Fixing Docker CE Install (Ainsible is broken)"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io 
+
 
 line_break "Running playbook"
 ansible-playbook main.yml
